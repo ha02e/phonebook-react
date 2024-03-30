494eeb1d73af6b6237e9a77313db93d4ae360821
hayoung <ha02e@naver.com>
2024-03-31 00:04:01 +0900

    유효성 검사 추가

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -24,6 +24,7 @@ const ContactForm = () => {
           <Form.Control
             type="text"
             placeholder="이름을 입력해주세요."
+            required
             aria-label="formName"
             aria-describedby="basic-addon1"
             size="lg"
@@ -34,8 +35,10 @@ const ContactForm = () => {
         <InputGroup className="mb-3" controlId="formContact">
           <InputGroup.Text id="basic-addon2">전화번호</InputGroup.Text>
           <Form.Control
-            type="number"
-            placeholder="전화번호를 입력해주세요."
+            type="tel"
+            pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
+            placeholder="전화번호를 입력해주세요. (010-0000-0000)"
+            required
             aria-label="formContact"
             aria-describedby="basic-addon2"
             size="lg"
